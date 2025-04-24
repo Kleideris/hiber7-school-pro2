c9f3d4f636a2ccb04a69210a675ec0f33ed06f6d
Ch. Kleideris <kleiderisch@gmail.com>
2025-04-24 21:07:24 +0300

    feat: updates

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,8 +1,14 @@
 package gr.aueb.cf;
 
+import gr.aueb.cf.core.enums.GenderType;
+import gr.aueb.cf.model.Region;
+import gr.aueb.cf.model.Teacher;
+import gr.aueb.cf.model.TeacherMoreInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.time.LocalDateTime;
 
 /**
  * Hello world!
@@ -13,7 +19,25 @@ public class App {
 
     public static void main(String[] args) {
 
+        em.getTransaction().begin();
 
+        Teacher teacher = Teacher.builder()
+                        .firstname("Κώστας")
+                        .lastname("Γιαννούτσος")
+                        .isActive(true)
+                        .build();
+
+        TeacherMoreInfo teacherMoreInfo = TeacherMoreInfo.builder()
+                        .dateOfBirth(LocalDateTime.of(2000, 2, 1, 10, 10, 10))
+                        .gender(GenderType.MALE)
+                        .build();
+
+        teacher.setTeacherMoreInfo(teacherMoreInfo);
+        Region region = em.find(Region.class, 1);
+        teacher.addRegion(region);
+
+        em.persist(teacher);
+        em.getTransaction().commit();
 
 
         em.close();
